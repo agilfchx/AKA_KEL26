93a9ae7d8c5262beff4aeb4343daf9b09a5b7843
Agil <agilfachrian2@gmail.com>
2020-12-23 23:04:18 +0700

    tubes aka

---
+++ Aplikasi/gnomeSort/gnomesort.py
@@ -0,0 +1,36 @@
+import time
+from numpy import random
+
+panjang = int(input("Masukkan Panjang Array-nya: "))
+
+#Awal Timer
+start = time.time()
+
+#Gnome Sort Code
+def gnomeSort(arr, n): 
+    index = 0
+    while index < n: 
+        if index == 0: 
+            index = index + 1
+        if arr[index] >= arr[index - 1]: 
+            index = index + 1
+        else: 
+            arr[index], arr[index-1] = arr[index-1], arr[index] 
+            index = index - 1
+  
+    return arr 
+
+#Proses Pemasukan Sorting
+data = random.randint(1000,size=(panjang))
+n = len(data)
+print("Index Sebelum di Sorting :",data)
+arr = gnomeSort(data, n) 
+
+#Berakhirnya Timer
+end = time.time()
+
+#Output
+print ("Index Setelah di Sorting :",data)
+print(f"Running Time Gnome Sort dengan panjang", panjang, "adalah", end - start)
+
+ok = input()
---
+++ README.txt
@@ -0,0 +1,4 @@
+Sumber Aplikasi Visualisasi 
+https://github.com/fraserlove/python-algorithms-and-simulations
+
+Thanks to Fraser
